9dfa17a75bd4dae2a538fd8b690b054d9f3e8012
Nathalia Lorena <nathalialorenacardoso@gmail.com>
2022-02-20 17:54:21 -0300

    Percorrendo arestas
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

--- src/roteadores.py
+++ src/roteadores.py
@@ -4,3 +4,13 @@ m = int(m)
 
 H = []
 n_out = [[]*n for i in range(n)]
+
+for j in range(m):
+    a, b, c = input().split()
+    a = int(a)
+    b = int(b)
+    c = int(c)
+
+    n_out[a].append((b, c))
+    n_out[b].append((a, c))
+    
